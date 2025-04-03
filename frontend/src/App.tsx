4aedaa376c5fb267d893930d436e5f35c25a9e3e
varmir1 <neilvargascalle@elpuig.xeill.net>
2025-04-03 16:09:29 +0200

    feat: new simple home page created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import TriviaGame from './components/TriviaGame';
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-      <main>
-        <TriviaGame />
-      </main>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/trivia" element={<TriviaGame />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
