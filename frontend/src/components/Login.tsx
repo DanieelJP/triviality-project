1803ac074b07de738b53da93b8fe98e137bad766
varmir1 <neilvargascalle@elpuig.xeill.net>
2025-04-08 20:34:53 +0200

    build: initial login pages

---
+++ frontend/src/components/Login.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import './Login.css';
+import logo from '../logo.png';
+
+    <div className="trivia-container">
+        <div className="trivia-card">
+            
+            <div className="tags-container">
+                
+            </div>
+                
+            </div>
+        </div>
